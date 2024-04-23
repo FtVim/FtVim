b51cf4a46871f97b6c0bb1757f0733871862484d
Bautista Rodriguez <bautirodri@gmail.com>
2024-04-23 23:41:49 +0200

    Telescope 🚀

--- lua/ftvim/plugins/telescope.lua
+++ lua/ftvim/plugins/telescope.lua
@@ -33,6 +33,5 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
