d9c0c8bfcc627e99337da93187a3d40dc8d33090
Bautista Rodriguez <bautirodri@gmail.com>
2024-04-23 22:47:46 +0200

    Telescope 🚀

--- lua/ftvim/plugins/telescope.lua
+++ lua/ftvim/plugins/telescope.lua
@@ -12,19 +12,12 @@ return {
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
+
     -- or create your custom action
 
     telescope.setup({
       defaults = {
         path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
-          },
-        },
       },
     })
 
