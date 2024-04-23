7d14f1ee5f327746dde81be4d6ef0bf5f6b73169
Bautista Rodriguez <bautirodri@gmail.com>
2024-04-23 22:17:03 +0200

    Dashboard 💩

--- lua/ftvim/plugins/dashboard.lua
+++ lua/ftvim/plugins/dashboard.lua
@@ -23,7 +23,23 @@ return {
       [[                                                                       ]],
     }
 
+    -- Set menu
+    dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
+        dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
+        dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
+        dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
+        dashboard.button("c", " " .. " Config",          "<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"),
+        dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+        dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
+        dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+    }
+    -- Send config to alpha
     alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
   end,
 }
 
