b45608c441333ace8659176fe4a24acc20ac9880
Santito <Santito@noreply>
2024-04-23 19:44:16 +0200

    🔖 COLORSHEME

---
+++ lua/ftvim/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+	-- tokyonight
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = {
+			style = "moon",
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+}
