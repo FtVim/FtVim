b0a236bfbc1bc30141531415e001588cd51ab8b9
Bautista Rodriguez <bautirodri@gmail.com>
2024-04-23 23:40:10 +0200

    Telescope 🚀

--- lua/ftvim/plugins/telescope.lua
+++ lua/ftvim/plugins/telescope.lua
@@ -11,15 +11,6 @@ return {
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.providers.telescope")
-
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
 
     telescope.setup({
       defaults = {
@@ -28,8 +19,6 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
           },
         },
       },
@@ -40,7 +29,7 @@ return {
     -- set keymaps
     local keymap = vim.keymap 
 
-    keymap.set("n", "<leader>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
