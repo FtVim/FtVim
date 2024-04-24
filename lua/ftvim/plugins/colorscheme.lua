ca353c3f80532c6f0157e474cd0a4001d3b67cb0
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-04-24 10:40:09 +0200

    Colors 🎨

--- lua/ftvim/plugins/colorscheme.lua
+++ lua/ftvim/plugins/colorscheme.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
-		init = function()
-			vim.cmd.colorscheme("tokyonight-moon")
-		end,
 		opts = {
 			style = "moon",
 			transparent = true,
