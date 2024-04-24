74964fa1dfda2f139943f20c2023949cf2da3bda
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-04-24 13:02:57 +0200

    Indent-blankline 🔥

--- lua/ftvim/plugins/indent-blankline.lua
+++ lua/ftvim/plugins/indent-blankline.lua
@@ -1,8 +1,27 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
-  opts = {
-    indent = { char = "|" },
-  },
+    "lukas-reineke/indent-blankline.nvim",
+    event = "LazyFile",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
 }
