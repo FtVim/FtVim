de7024a70bf5529ed0642452f975ad0407c5e39f
Santito <68120901+San-tito@users.noreply.github.com>
2024-04-24 10:47:18 +0200

    Update colorscheme.lua 💥

--- lua/ftvim/plugins/colorscheme.lua
+++ lua/ftvim/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("tokyonight-moon")
+		end,
 		opts = {
 			style = "moon",
 			transparent = true,
