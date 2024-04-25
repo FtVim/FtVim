542ed0ce801a0487501feb14bde489917562ba89
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-04-25 15:35:34 +0200

    Toggleterm 📈

---
+++ lua/ftvim/plugins/toggleterm.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        cmd = { "ToggleTerm" },
+        config = true,
+        keys = {
+            {
+              "<leader>tv",
+              function()
+                local count = vim.v.count1
+                require("toggleterm").toggle(count, 70, vim.loop.cwd(), "vertical")
+              end,
+              desc = "ToggleTerm (vertical)",
+            },
+            {
+              "<leader>th",
+              function()
+                local count = vim.v.count1
+                require("toggleterm").toggle(count, 15, vim.loop.cwd(), "horizontal")
+              end,
+              desc = "ToggleTerm (horizontal)",
+            },
+            {
+              "<leader>tf",
+              function()
+                local count = vim.v.count1
+                require("toggleterm").toggle(count, 5, vim.loop.cwd(), "float")
+              end,
+              desc = "ToggleTerm (floating)",
+            },
+        },
+    }
+}
