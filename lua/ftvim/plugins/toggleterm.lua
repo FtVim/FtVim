88da8407233627f3adec1bda75300509c7c56f7d
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-04-25 14:03:24 +0200

    Toggleterm 📈

--- lua/ftvim/plugins/toggleterm.lua
+++ lua/ftvim/plugins/toggleterm.lua
@@ -19,28 +19,26 @@ return {
               end,
               desc = "ToggleTerm (horizontal)",
             },
-            require("toggleterm").setup{
-                opts = {
-                    -- size can be a number or function which is passed the current terminal
-                    size = function(term)
-                        if term.direction == "horizontal" then
-                            return 15
-                        elseif term.direction == "vertical" then
-                            return 20
-                        end
-                    end,
-                    open_mapping = [[<c-\>]],
-                  hide_numbers = true, -- hide the number column in toggleterm buffers
-                  shade_filetypes = {},
-                  shade_terminals = true,
-                  start_in_insert = true,
-                  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-                  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-                  persist_size = true,
-                  direction = "horizontal" or "vertical" or "window" or "float",
-                  close_on_exit = true, -- close the terminal window when the process exits
-                },
+            opts = {
+                -- size can be a number or function which is passed the current terminal
+                size = function(term)
+                    if term.direction == "horizontal" then
+                        return 15
+                    elseif term.direction == "vertical" then
+                        return vim.o.columns * 152
+                    end
+                end,
+                open_mapping = [[<c-\>]],
+              hide_numbers = true, -- hide the number column in toggleterm buffers
+              shade_filetypes = {},
+              shade_terminals = true,
+              start_in_insert = true,
+              insert_mappings = true, -- whether or not the open mapping applies in insert mode
+              terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+              persist_size = true,
+              direction = "horizontal" or "vertical" or "window" or "float",
+              close_on_exit = true, -- close the terminal window when the process exits
             },
         },
-    },
+    }
 }
