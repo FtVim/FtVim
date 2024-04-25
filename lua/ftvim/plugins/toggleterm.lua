519d9e5f83c01163c71982d58f1b10510da474a8
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-04-25 11:35:24 +0200

    Toggleterm 📈

--- lua/ftvim/plugins/toggleterm.lua
+++ lua/ftvim/plugins/toggleterm.lua
@@ -1,5 +1,26 @@
 return {
-      'akinsho/toggleterm.nvim',
-      version = "*",
-      config = true
+  "akinsho/toggleterm.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("toggleterm").setup()
+
+    local Terminal = require("toggleterm.terminal").Terminal
+    local colors = require("config.colors").colors
+    local defaults = {
+      direction = "float",
+      float_opts = {
+        border = "single",
+      },
+      shade_terminals = false,
+      highlights = {
+        Normal = {
+          guibg = colors.grey14,
+        },
+        FloatBorder = {
+          guibg = colors.grey14,
+          guifg = colors.grey14,
+        },
+      },
+    }
+  end,
 }
