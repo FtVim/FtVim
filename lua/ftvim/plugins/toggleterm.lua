fa368ba1b9a6c3b7a031a3c63837cdf6a2aef0c7
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-04-25 13:38:27 +0200

    Toggleterm 📈

--- lua/ftvim/plugins/toggleterm.lua
+++ lua/ftvim/plugins/toggleterm.lua
@@ -1,9 +1,17 @@
 return {
         'akinsho/toggleterm.nvim',
         version = "*",
+        config = true,
+        cmd = 'ToggleTerm',
+        keys = {{ '<C-/', '<cmd>ToggleTerm<cr>', desc = 'Toggle floating term'}},
         opts = {
-            direction = "float",
-            open_mapping = [[c-\>]],
+            direction = 'float',
+            open_mapping = [[C-/>]],
+            shade_filetypes = {},
+            hide_numbers = true,
+            insert_mappings = true,
+            terminal_mappings = true,
+            start_in_insert = true,
+            close_on_exit = true,
         },
-        config = true
 }
