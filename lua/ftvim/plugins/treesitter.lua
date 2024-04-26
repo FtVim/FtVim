4e0c28913241a9007a7afb7626ee3e52826aca2a
Bauti <bautirodri@gmal.com>
2024-04-26 21:39:58 +0200

    Telescope 🚀 and treesitter 🌱

--- lua/ftvim/plugins/treesitter.lua
+++ lua/ftvim/plugins/treesitter.lua
@@ -26,27 +26,10 @@ return {
             "regex",
             "toml",
             "tsx",
-            "typescript",
             "vim",
             "vimdoc",
-            "xml",
             "yaml",
           },
-    },
-    keys = {
-      {
-        "<leader>ut",
-        function()
-          local tsc = require("treesitter-context")
-          tsc.toggle()
-          if LazyVim.inject.get_upvalue(tsc.toggle, "enabled") then
-            LazyVim.info("Enabled Treesitter Context", { title = "Option" })
-          else
-            LazyVim.warn("Disabled Treesitter Context", { title = "Option" })
-          end
-        end,
-        desc = "Toggle Treesitter Context",
-      },
     },
   },
 }
