1611d5ca64641efb2a7abb83d9aef815aa1af3a9
Bautista Rodriguez Ramal <bautrodr@cbr1s3.42barcelona.com>
2024-05-02 17:15:53 +0200

    nvim-cmp 📌

--- lua/ftvim/plugins/nvim-cmp.lua
+++ lua/ftvim/plugins/nvim-cmp.lua
@@ -8,8 +8,6 @@ return {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
       version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-      -- install jsregexp (optional!).
-      build = "make install_jsreddgexp",
     },
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
