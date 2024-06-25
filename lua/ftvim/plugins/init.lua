860192a0f627e4738ccc12adbedbb7bcd8cdb78b
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-25 23:40:51 +0200

    haber

--- lua/ftvim/plugins/init.lua
+++ lua/ftvim/plugins/init.lua
@@ -1,10 +1,11 @@
-if vim.fn.has("nvim-0.9") ~= 1 then
-	vim.notify("Please upgrade your Neovim base installation. FtVim requires v0.9+", vim.log.levels.WARN)
-	vim.wait(5000, function()
-		---@diagnostic disable-next-line: redundant-return-value
-		return false
-	end)
-	vim.cmd("cquit")
+if vim.fn.has("nvim-0.9.0") == 0 then
+  vim.api.nvim_echo({
+    { "Ft requires Neovim >= 0.9.0\n", "ErrorMsg" },
+    { "Press any key to exit", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+  return {}
 end
 
 require("ftvim.bootstrap"):init()
