f76b6d278c994b4004102cb235dd0e4718a7fd61
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-25 17:39:12 +0200

    commit por commit

--- lua/ftvim/init.lua
+++ lua/ftvim/init.lua
@@ -1,1 +1,10 @@
+if vim.fn.has "nvim-0.10" ~= 1 then
+  vim.notify("Please upgrade your Neovim base installation. Lunarvim requires v0.10+", vim.log.levels.WARN)
+  vim.wait(5000, function()
+    ---@diagnostic disable-next-line: redundant-return-value
+    return false
+  end)
+  vim.cmd "cquit"
+end
+
 require("ftvim.bootstrap"):init()
