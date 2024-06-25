b1d0451552424cd958a3d147818fdfa6af11b6f3
Santito <68120901+San-tito@users.noreply.github.com>
2024-06-25 23:44:45 +0200

    Update bootstrap.lua

--- lua/ftvim/bootstrap.lua
+++ lua/ftvim/bootstrap.lua
@@ -1,45 +1,7 @@
 local M = {}
 
-if vim.fn.has "nvim-0.10" ~= 1 then
-  vim.notify("Please upgrade your Neovim base installation. Lunarvim requires v0.10+", vim.log.levels.WARN)
-  vim.wait(5000, function()
-    ---@diagnostic disable-next-line: redundant-return-value
-    return false
-  end)
-  vim.cmd "cquit"
-end
-
-local uv = vim.loop
-local path_sep = uv.os_uname().version:match "Windows" and "\\" or "/"
-
----Join path segments that were passed as input
----@return string
-function _G.join_paths(...)
-  local result = table.concat({ ... }, path_sep)
-  return result
-end
-
-_G.require_clean = require("lvim.utils.modules").require_clean
-_G.require_safe = require("lvim.utils.modules").require_safe
-_G.reload = require("lvim.utils.modules").reload
-
----Get the full path to `$LUNARVIM_RUNTIME_DIR`
----@return string|nil
-function _G.get_runtime_dir()
-  local lvim_runtime_dir = os.getenv "LUNARVIM_RUNTIME_DIR"
-  if not lvim_runtime_dir then
-    -- when nvim is used directly
-    return vim.call("stdpath", "data")
-  end
-  return lvim_runtime_dir
-end
-
-local M = {}
-
 function M:init(base_dir)
-
   require("ftvim.config"):init()
-
   return self
 end
 
