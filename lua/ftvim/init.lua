f02585d06b00d2c53f01f20612c2f769eb0a8e05
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-25 17:40:25 +0200

    commit por commit

--- lua/ftvim/init.lua
+++ lua/ftvim/init.lua
@@ -1,4 +1,4 @@
-if vim.fn.has "nvim-0.10" ~= 1 then
+if vim.fn.has "nvim-0.10" ~= 0 then
   vim.notify("Please upgrade your Neovim base installation. Lunarvim requires v0.10+", vim.log.levels.WARN)
   vim.wait(5000, function()
     ---@diagnostic disable-next-line: redundant-return-value
