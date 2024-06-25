519faded46ff62024ae6c8da259fc443200b9a96
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-26 01:21:38 +0200

    Revert "Revert "locura dashboard""
    This reverts commit 8bdddf0930d83338a726017d49fea1c25ab895be.

--- lua/ftvim/core/alpha/dashboard.lua
+++ lua/ftvim/core/alpha/dashboard.lua
@@ -54,8 +54,8 @@ function M.get_sections()
 		entries = {
 			{ "f", ftvim.icons.ui.FindFile .. "  Find File", "<CMD>Telescope find_files<CR>" },
 			{ "n", ftvim.icons.ui.NewFile .. "  New File", "<CMD>ene!<CR>" },
-			{ "r", ftvim.icons.ui.Files .. " Recent files", "<CMD> Telescope oldfiles <CR>" },
-			{ "s", ftvim.icons.ui.Forward .. " Restore Session", [[<CMD> lua require("persistence").load() <CR>]] },
+			{ "r", ftvim.icons.ui.Files .. "  Recent files", "<CMD> Telescope oldfiles <CR>" },
+			{ "s", ftvim.icons.ui.Forward .. "  Restore Session", [[<CMD> lua require("persistence").load() <CR>]] },
 			{ "p", ftvim.icons.ui.Project .. "  Projects ", "<CMD>Telescope projects<CR>" },
 			{ "r", ftvim.icons.ui.History .. "  Recent files", ":Telescope oldfiles <CR>" },
 			{ "g", ftvim.icons.ui.FindText .. "  Find Text", "<CMD>Telescope live_grep<CR>" },
