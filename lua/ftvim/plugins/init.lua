56a3f74a3d7db9abcf4588d0ea498f07e562b124
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-25 17:44:35 +0200

    maybe

---
+++ lua/ftvim/plugins/init.lua
@@ -0,0 +1,14 @@
+if vim.fn.has("nvim-0.10.0") == 0 then
+  vim.api.nvim_echo({
+    { "FtVim requires Neovim >= 0.9.0\n", "ErrorMsg" },
+    { "Press any key to exit", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+  return {}
+end
+
+return {
+  { "folke/lazy.nvim", version = "*" },
+  { "FtVim/FtVim", priority = 10000, lazy = false, version = "*" },
+}
