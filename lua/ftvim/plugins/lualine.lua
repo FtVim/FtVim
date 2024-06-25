282bf2ec6fb4adbae8e5204bac6e9debc907668c
Bautista Rodriguez <bautirodri@gmail.com>
2024-06-25 21:43:58 +0200

    lualine

---
+++ lua/ftvim/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local lualine = require("lualine")
+    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+    -- configure lualine with modified theme
+    lualine.setup({
+      options = {
+        theme = 'auto',
+      },
+      sections = {
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
+        },
+      },
+    })
+  end,
+}
