4675031633b6bc41ef3bb5d99395131593db726b
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-27 01:04:01 +0200

    feature updates

--- lua/ftvim/bootstrap.lua
+++ lua/ftvim/bootstrap.lua
@@ -1,15 +1,5 @@
 local M = {}
 
-if vim.fn.has("nvim-0.9.0") == 0 then
-	vim.api.nvim_echo({
-		{ "FtVim requires Neovim >= 0.9.0\n", "ErrorMsg" },
-		{ "Press any key to exit", "MoreMsg" },
-	}, true, {})
-	vim.fn.getchar()
-	vim.cmd([[quit]])
-	return {}
-end
-
 local uv = vim.loop
 local path_sep = uv.os_uname().version:match("Windows") and "\\" or "/"
 
