a4c85e46967401303c45c8a783fe421ea480963f
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-27 01:11:21 +0200

    feature updates

--- lua/ftvim/plugins/init.lua
+++ lua/ftvim/plugins/init.lua
@@ -1,2 +1,6 @@
 require("ftvim.bootstrap"):init()
 
+return {
+  { "folke/lazy.nvim", version = "*" },
+  { "LazyVim/LazyVim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
+}
