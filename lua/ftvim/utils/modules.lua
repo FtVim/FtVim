f0a8221c752cf361769cae5c1d4710a9d38b7271
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-28 00:46:55 +0200

    which-key

--- lua/ftvim/utils/modules.lua
+++ lua/ftvim/utils/modules.lua
@@ -1,5 +1,47 @@
 local M = {}
 
+local function _assign(old, new, k)
+	local otype = type(old[k])
+	local ntype = type(new[k])
+	if (otype == "thread" or otype == "userdata") or (ntype == "thread" or ntype == "userdata") then
+		vim.notify(string.format("warning: old or new attr %s type be thread or userdata", k))
+	end
+	old[k] = new[k]
+end
+
+local function _replace(old, new, repeat_tbl)
+	if repeat_tbl[old] then
+		return
+	end
+	repeat_tbl[old] = true
+
+	local dellist = {}
+	for k, _ in pairs(old) do
+		if not new[k] then
+			table.insert(dellist, k)
+		end
+	end
+	for _, v in ipairs(dellist) do
+		old[v] = nil
+	end
+
+	for k, _ in pairs(new) do
+		if not old[k] then
+			old[k] = new[k]
+		else
+			if type(old[k]) ~= type(new[k]) then
+				_assign(old, new, k)
+			else
+				if type(old[k]) == "table" then
+					_replace(old[k], new[k], repeat_tbl)
+				else
+					_assign(old, new, k)
+				end
+			end
+		end
+	end
+end
+
 M.require_clean = function(m)
 	package.loaded[m] = nil
 	_G[m] = nil
