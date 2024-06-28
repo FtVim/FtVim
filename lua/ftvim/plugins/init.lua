c2aa48d77de5396211c19e662b96b8e9266824d0
Bautista Rodriguez <bautirodri@gmail.com>
2024-06-28 15:56:03 +0200

    plugin-loader

--- lua/ftvim/plugins/init.lua
+++ lua/ftvim/plugins/init.lua
@@ -2,6 +2,9 @@ vim.notify("Starting FtVim")
 
 require("ftvim.bootstrap"):init()
 
+local plugins = require "ftvim.core-plugins"
+require("ftvim.plugin-loader").load { plugins, ftvim.plugins }
+
 vim.g.mapleader = (ftvim.leader == "space" and " ") or ftvim.leader
 
 require("ftvim.core.theme").setup()
