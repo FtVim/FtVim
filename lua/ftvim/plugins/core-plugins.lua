72a2d9660f77ba21a4a29a005bfb309e1e4c8427
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-04 16:21:37 +0200

    added mini.indentscope

--- lua/ftvim/plugins/core-plugins.lua
+++ lua/ftvim/plugins/core-plugins.lua
@@ -382,8 +382,8 @@ return {
 
 		version = false,
 		opts = {
-			-- symbol = "▏",
-			symbol = "│",
+			symbol = "▏",
+			-- symbol = "│",
 			options = { try_as_border = true },
 		},
 		init = function()
