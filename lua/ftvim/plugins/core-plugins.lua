74e0f5f97ef8c416b92ed0e5804a539ec5de1f69
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-07 13:32:41 +0200

    persistence

--- lua/ftvim/plugins/core-plugins.lua
+++ lua/ftvim/plugins/core-plugins.lua
@@ -413,13 +413,6 @@ return {
 	{
 		"folke/persistence.nvim",
 		config = true,
-		-- stylua: ignore
-		keys = {
-			{ "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-			{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-			{ "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-		},
-
 		-- library used by other plugins
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
