c162621ba8e200bf308e1fc416283f59d1c85a1f
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-08 00:08:31 +0200

    persistence

--- lua/ftvim/core/persistence.lua
+++ lua/ftvim/core/persistence.lua
@@ -6,11 +6,11 @@ M.config = function()
 		opts = {
 			dir = vim.fn.expand(vim.fn.stdpath('state') .. '/sessions/'),
 			options = vim.opt.sessionoptions:get(),
-		},
-		keys = {
-			{ "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-			{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-			{ "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+			keys = {
+				{ "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+				{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+				{ "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+			},
 		},
 	}
 end
