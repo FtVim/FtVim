592f2e64935e83b7ea580cd6c86d0a4981fcbe23
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-07 13:37:09 +0200

    persistence

--- lua/ftvim/plugins/core-plugins.lua
+++ lua/ftvim/plugins/core-plugins.lua
@@ -412,7 +412,19 @@ return {
 
 	{
 		"folke/persistence.nvim",
-		config = true,
+		event = "BufReadPre",
+		print(vim.fn.stdpath("state") .. "/sessions/") -- directory where session files are saved
+		-- minimum number of file buffers that need to be open to save
+		-- Set to 0 to always save
+		branch = false, -- use git branch to save session
+		opts = { options = vim.opt.sessionoptions:get() },
+		-- stylua: ignore
+		keys = {
+			{ "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+			{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+			{ "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+		},
+
 		-- library used by other plugins
 		{ "nvim-lua/plenary.nvim", lazy = true },
 	},
