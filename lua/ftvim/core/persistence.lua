58fe10ad3a538c2d711bf8e1759288e0709a6719
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-08 01:23:17 +0200

    persistence hdp

--- lua/ftvim/core/persistence.lua
+++ lua/ftvim/core/persistence.lua
@@ -3,32 +3,12 @@ local M = {}
 M.config = function()
 	ftvim.builtin.persistence = {
 		enable = true,
-		opts = {
-			options = {
-				keys = {
-					{
-						"<leader>qs",
-						function()
-							require("persistence").load()
-						end,
-						desc = "Restore Session",
-					},
-					{
-						"<leader>ql",
-						function()
-							require("persistence").load({ last = true })
-						end,
-						desc = "Restore Last Session",
-					},
-					{
-						"<leader>qd",
-						function()
-							require("persistence").stop()
-						end,
-						desc = "Don't Save Current Session",
-					},
-				},
-			},
+		opts = { options = vim.opt.sessionoptions:get() },
+		-- stylua: ignore
+		keys = {
+			{ "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+			{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+			{ "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
 		},
 	}
 end
