0671832ce722d99c6b764e1585dc63c122482af7
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-10 23:40:41 +0200

    fixes

--- lua/ftvim/plugins/core-plugins.lua
+++ lua/ftvim/plugins/core-plugins.lua
@@ -88,32 +88,6 @@ return {
 
 	-- Install nvim-cmp, and buffer source as a dependency
 
-	 {
-
-	 	"hrsh7th/nvim-cmp",
-
-	 	config = function()
-			if ftvim.builtin.cmp then
-	 			require("ftvim.core.cmp").setup()
-	 		end
-	 	end,
-
-	 	event = { "InsertEnter", "CmdlineEnter" },
-
-	 	dependencies = {
-
-	 		"cmp-nvim-lsp",
-
-	 		"cmp_luasnip",
-
-	 		"cmp-buffer",
-
-	 		"cmp-path",
-
-	 		"cmp-cmdline",
-	 	},
-	 },
-
 	{ "hrsh7th/cmp-nvim-lsp", lazy = true },
 
 	{ "saadparwaiz1/cmp_luasnip", lazy = true },
