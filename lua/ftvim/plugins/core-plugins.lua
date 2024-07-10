6b351252c5071d2024865e87662954c5391ac268
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-10 22:55:51 +0200

    lsp

--- lua/ftvim/plugins/core-plugins.lua
+++ lua/ftvim/plugins/core-plugins.lua
@@ -40,6 +40,8 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.clangd.setup({})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 
