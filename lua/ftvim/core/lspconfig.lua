48af76ad3970c2dbf2c39f5183415322db19b562
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-11 00:18:08 +0200

    fixes

--- lua/ftvim/core/lspconfig.lua
+++ lua/ftvim/core/lspconfig.lua
@@ -1,38 +1,19 @@
 local M = {}
 
-M.config = function()
-	local capabilities = require("cmp_nvim_lsp").default_capabilities()
-	ftvim.builtin.lspconfig = {
-		active = true,
-		servers = {
-			lua_ls = {
-				capabilities = capabilities,
-			},
-			clangd = {
-				capabilities = capabilities,
-			},
-		},
-	}
-end
-
 M.setup = function()
-	local status_ok, lspconfig = pcall(require, "lspconfig")
-	if not status_ok then
-		print("Error loading nvim-lspconfig")
-		return
-	end
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	for server, config in pairs(ftvim.builtin.lspconfig.servers) do
-		config.capabilities = capabilities
-		lspconfig[server].setup(config)
-	end
+    lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+    })
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    lspconfig.clangd.setup({
+        capabilities = capabilities,
+    })
 
-	if ftvim.builtin.lspconfig.on_config_done then
-		ftvim.builtin.lspconfig.on_config_done()
-	end
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 end
 
 return M
