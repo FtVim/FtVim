0749fe6c22cea153378dcac49936d3793b6d0a7b
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-11 00:23:24 +0200

    fixes

--- lua/ftvim/core/lspconfig.lua
+++ lua/ftvim/core/lspconfig.lua
@@ -1,15 +1,30 @@
 local M = {}
 
-M.config = function(config)
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+M.setup = function()
+    local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+    if not status_ok then
+        return
+    end
+
+    M.capabilities = cmp_nvim_lsp.default_capabilities()
+end
+
+M.config = function()
+    local status_ok, lspconfig = pcall(require, "lspconfig")
+    if not status_ok then
+        return
+    end
+
+    if not M.capabilities then
+        M.setup()
+    end
 
     lspconfig.lua_ls.setup({
-        capabilities = capabilities,
+        capabilities = M.capabilities,
     })
 
     lspconfig.clangd.setup({
-        capabilities = capabilities,
+        capabilities = M.capabilities,
     })
 
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
@@ -17,3 +32,4 @@ M.config = function(config)
 end
 
 return M
+
