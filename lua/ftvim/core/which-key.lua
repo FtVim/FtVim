bdc3064cc03a9777a5e8469657bd7fcd9da83e12
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-13 13:27:36 +0200

    which

--- lua/ftvim/core/which-key.lua
+++ lua/ftvim/core/which-key.lua
@@ -1,8 +1,8 @@
 local M = {}
-local wk = require("whick-key")
 
 M.config = function()
 	ftvim.builtin.which_key = {
+		---@usage disable which-key completely [not recommended]
 		active = true,
 		on_config_done = nil,
 		setup = {
@@ -28,7 +28,13 @@ M.config = function()
 			-- add operators that will trigger motion and text object completion
 			-- to enable all native operators, set the preset / operators plugin above
 			operators = { gc = "Comments" },
-			key_labels = {},
+			key_labels = {
+				-- override the label used to display some keys. It doesn't effect WK in any other way.
+				-- For example:
+				-- ["<space>"] = "SPC",
+				-- ["<cr>"] = "RET",
+				-- ["<tab>"] = "TAB",
+			},
 			icons = {
 				breadcrumb = ftvim.icons.ui.DoubleChevronRight, -- symbol used in the command line area that shows your active key combo
 				separator = ftvim.icons.ui.BoldArrowRight, -- symbol used between a key and it's label
@@ -71,6 +77,159 @@ M.config = function()
 				filetypes = { "TelescopePrompt" },
 			},
 		},
+
+		opts = {
+			mode = "n", -- NORMAL mode
+			prefix = "<leader>",
+			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+			silent = true, -- use `silent` when creating keymaps
+			noremap = true, -- use `noremap` when creating keymaps
+			nowait = true, -- use `nowait` when creating keymaps
+		},
+		vopts = {
+			mode = "v", -- VISUAL mode
+			prefix = "<leader>",
+			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+			silent = true, -- use `silent` when creating keymaps
+			noremap = true, -- use `noremap` when creating keymaps
+			nowait = true, -- use `nowait` when creating keymaps
+		},
+		-- NOTE: Prefer using : over <cmd> as the latter avoids going back in normal-mode.
+		-- see https://neovim.io/doc/user/map.html#:map-cmd
+		vmappings = {
+			["/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment toggle linewise (visual)" },
+			g = {
+				name = "Git",
+				r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset Hunk" },
+				s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage Hunk" },
+			},
+		},
+		mappings = {
+			[";"] = { "<cmd>Alpha<CR>", "Dashboard" },
+			["w"] = { "<cmd>w!<CR>", "Save" },
+			["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
+			["bd"] = { "<cmd>bdelete<CR>", "Close Buffer" },
+			["f"] = {
+				function()
+					require("ftvim.core.telescope.finders").find_project_files({ previewer = false })
+				end,
+				"Find File",
+			},
+			["e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
+			q = {
+				name = "Quit",
+				q = { "<cmd>confirm q<CR>", "Quit" },
+				-- s = {
+				-- 	function()
+				-- 		require("ftvim.core.persistence").load()
+				-- 	end,
+				-- 	"Restore Session",
+				-- },
+			},
+			b = {
+				name = "Buffers",
+				j = { "<cmd>BufferLinePick<cr>", "Jump" },
+				f = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+				b = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+				n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+				W = { "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)" },
+				e = {
+					"<cmd>BufferLinePickClose<cr>",
+					"Pick which buffer to close",
+				},
+				h = { "<cmd>BufferLineCloseLeft<cr>", "Close all to the left" },
+				l = {
+					"<cmd>BufferLineCloseRight<cr>",
+					"Close all to the right",
+				},
+				D = {
+					"<cmd>BufferLineSortByDirectory<cr>",
+					"Sort by directory",
+				},
+				L = {
+					"<cmd>BufferLineSortByExtension<cr>",
+					"Sort by language",
+				},
+			},
+			c = {
+				name = "Code",
+				m = { "<cmd>Mason<cr>", "Mason" },
+				a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
+			},
+			w = {
+				name = "Windows",
+				w = { "<cmd>split new<cr>", "Other Window" },
+				d = { "<cmd>q<cr>", "Delete Window" },
+				s = {
+					name = "Split",
+					h = { "<cmd>split<cr>", "Split Horizontal" },
+					v = { "<cmd>vsplit<cr>", "Split Vertical" },
+				},
+			},
+			p = {
+				name = "Plugins",
+				i = { "<cmd>Lazy install<cr>", "Install" },
+				s = { "<cmd>Lazy sync<cr>", "Sync" },
+				S = { "<cmd>Lazy clear<cr>", "Status" },
+				u = { "<cmd>Lazy update<cr>", "Update" },
+				l = { "<cmd>Lazy<cr>", "Log" },
+			},
+			g = {
+				name = "Git",
+				g = { "<cmd>lua require 'ftvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
+				j = { "<cmd>lua require 'gitsigns'.nav_hunk('next', {navigation_message = false})<cr>", "Next Hunk" },
+				k = { "<cmd>lua require 'gitsigns'.nav_hunk('prev', {navigation_message = false})<cr>", "Prev Hunk" },
+				l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+				L = { "<cmd>lua require 'gitsigns'.blame_line({full=true})<cr>", "Blame Line (full)" },
+				p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+				r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+				R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+				s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+				u = {
+					"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+					"Undo Stage Hunk",
+				},
+				o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+				b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+				c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+				C = {
+					"<cmd>Telescope git_bcommits<cr>",
+					"Checkout commit(for current file)",
+				},
+				d = {
+					"<cmd>Gitsigns diffthis HEAD<cr>",
+					"Git Diff",
+				},
+			},
+			F = {
+				name = "+FtVim",
+				k = { "<cmd>Telescope keymaps<cr>", "View FtVim's keymappings" },
+			},
+			f = {
+				name = "Find",
+				b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+				s = { "<cmd>Telescope live_grep<cr>", "Find string in cwd" },
+				c = { "<cmd>Telescope grep_string<cr>", "Find string under cursor in cwd" },
+				f = { "<cmd>Telescope find_files<cr>", "Find File" },
+				h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+				H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+				M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+				r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+				R = { "<cmd>Telescope registers<cr>", "Registers" },
+				t = { "<cmd>Telescope colorscheme<cr>", "Change theme" },
+				k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+				C = { "<cmd>Telescope commands<cr>", "Commands" },
+				l = { "<cmd>Telescope resume<cr>", "Resume last search" },
+				p = {
+					"<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+					"Colorscheme with Preview",
+				},
+			},
+			T = {
+				name = "Treesitter",
+				i = { ":TSConfigInfo<cr>", "Info" },
+			},
+		},
 	}
 end
 
@@ -79,286 +238,14 @@ M.setup = function()
 
 	which_key.setup(ftvim.builtin.which_key.setup)
 
-	wk.add({
-		{
-			"<leader>/",
-			"<Plug>(comment_toggle_linewise_current)",
-			desc = "Comment toggle current line",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>;", "<cmd>Alpha<CR>", desc = "Dashboard", nowait = true, remap = false },
-		{ "<leader>F", group = "FtVim", nowait = true, remap = false },
-		{
-			"<leader>Fk",
-			"<cmd>Telescope keymaps<cr>",
-			desc = "View FtVim's keymappings",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>T", group = "Treesitter", nowait = true, remap = false },
-		{ "<leader>Ti", ":TSConfigInfo<cr>", desc = "Info", nowait = true, remap = false },
-		{ "<leader>b", group = "Buffers", nowait = true, remap = false },
-		{
-			"<leader>bD",
-			"<cmd>BufferLineSortByDirectory<cr>",
-			desc = "Sort by directory",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>bL",
-			"<cmd>BufferLineSortByExtension<cr>",
-			desc = "Sort by language",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>bW",
-			"<cmd>noautocmd w<cr>",
-			desc = "Save without formatting (noautocmd)",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>bb", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous", nowait = true, remap = false },
-		{ "<leader>bd", "<cmd>bdelete<CR>", desc = "Close Buffer", nowait = true, remap = false },
-		{
-			"<leader>be",
-			"<cmd>BufferLinePickClose<cr>",
-			desc = "Pick which buffer to close",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>bf",
-			"<cmd>Telescope buffers previewer=false<cr>",
-			desc = "Find",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>bh",
-			"<cmd>BufferLineCloseLeft<cr>",
-			desc = "Close all to the left",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>bj", "<cmd>BufferLinePick<cr>", desc = "Jump", nowait = true, remap = false },
-		{
-			"<leader>bl",
-			"<cmd>BufferLineCloseRight<cr>",
-			desc = "Close all to the right",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>bn", "<cmd>BufferLineCycleNext<cr>", desc = "Next", nowait = true, remap = false },
-		{ "<leader>c", group = "Code", nowait = true, remap = false },
-		{
-			"<leader>ca",
-			"<cmd>lua vim.lsp.buf.code_action()<cr>",
-			desc = "Code action",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>cm", "<cmd>Mason<cr>", desc = "Mason", nowait = true, remap = false },
-		{ "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Explorer", nowait = true, remap = false },
-		{ "<leader>f", group = "Find", nowait = true, remap = false },
-		{ "<leader>fC", "<cmd>Telescope commands<cr>", desc = "Commands", nowait = true, remap = false },
-		{
-			"<leader>fH",
-			"<cmd>Telescope highlights<cr>",
-			desc = "Find highlight groups",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>fM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages", nowait = true, remap = false },
-		{ "<leader>fR", "<cmd>Telescope registers<cr>", desc = "Registers", nowait = true, remap = false },
-		{
-			"<leader>fb",
-			"<cmd>Telescope git_branches<cr>",
-			desc = "Checkout branch",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>fc",
-			"<cmd>Telescope grep_string<cr>",
-			desc = "Find string under cursor in cwd",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", nowait = true, remap = false },
-		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find Help", nowait = true, remap = false },
-		{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps", nowait = true, remap = false },
-		{
-			"<leader>fl",
-			"<cmd>Telescope resume<cr>",
-			desc = "Resume last search",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>fp",
-			"<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
-			desc = "Colorscheme with Preview",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>fr",
-			"<cmd>Telescope oldfiles<cr>",
-			desc = "Open Recent File",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>fs",
-			"<cmd>Telescope live_grep<cr>",
-			desc = "Find string in cwd",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>ft", "<cmd>Telescope colorscheme<cr>", desc = "Change theme", nowait = true, remap = false },
-		{ "<leader>g", group = "Git", nowait = true, remap = false },
-		{
-			"<leader>gC",
-			"<cmd>Telescope git_bcommits<cr>",
-			desc = "Checkout commit(for current file)",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gL",
-			"<cmd>lua require 'gitsigns'.blame_line({full=true})<cr>",
-			desc = "Blame Line (full)",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gR",
-			"<cmd>lua require 'gitsigns'.reset_buffer()<cr>",
-			desc = "Reset Buffer",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gb",
-			"<cmd>Telescope git_branches<cr>",
-			desc = "Checkout branch",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gc",
-			"<cmd>Telescope git_commits<cr>",
-			desc = "Checkout commit",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", desc = "Git Diff", nowait = true, remap = false },
-		{
-			"<leader>gg",
-			"<cmd>lua require 'ftvim.core.terminal'.lazygit_toggle()<cr>",
-			desc = "Lazygit",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gj",
-			"<cmd>lua require 'gitsigns'.nav_hunk('next', {navigation_message = false})<cr>",
-			desc = "Next Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gk",
-			"<cmd>lua require 'gitsigns'.nav_hunk('prev', {navigation_message = false})<cr>",
-			desc = "Prev Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gl",
-			"<cmd>lua require 'gitsigns'.blame_line()<cr>",
-			desc = "Blame",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>go",
-			"<cmd>Telescope git_status<cr>",
-			desc = "Open changed file",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gp",
-			"<cmd>lua require 'gitsigns'.preview_hunk()<cr>",
-			desc = "Preview Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gr",
-			"<cmd>lua require 'gitsigns'.reset_hunk()<cr>",
-			desc = "Reset Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gs",
-			"<cmd>lua require 'gitsigns'.stage_hunk()<cr>",
-			desc = "Stage Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gu",
-			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-			desc = "Undo Stage Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>p", group = "Plugins", nowait = true, remap = false },
-		{ "<leader>pS", "<cmd>Lazy clear<cr>", desc = "Status", nowait = true, remap = false },
-		{ "<leader>pi", "<cmd>Lazy install<cr>", desc = "Install", nowait = true, remap = false },
-		{ "<leader>pl", "<cmd>Lazy<cr>", desc = "Log", nowait = true, remap = false },
-		{ "<leader>ps", "<cmd>Lazy sync<cr>", desc = "Sync", nowait = true, remap = false },
-		{ "<leader>pu", "<cmd>Lazy update<cr>", desc = "Update", nowait = true, remap = false },
-		{ "<leader>q", group = "Quit", nowait = true, remap = false },
-		{ "<leader>qq", "<cmd>confirm q<CR>", desc = "Quit", nowait = true, remap = false },
-		{ "<leader>w", group = "Windows", nowait = true, remap = false },
-		{ "<leader>wd", "<cmd>q<cr>", desc = "Delete Window", nowait = true, remap = false },
-		{ "<leader>ws", group = "Split", nowait = true, remap = false },
-		{ "<leader>wsh", "<cmd>split<cr>", desc = "Split Horizontal", nowait = true, remap = false },
-		{ "<leader>wsv", "<cmd>vsplit<cr>", desc = "Split Vertical", nowait = true, remap = false },
-		{ "<leader>ww", "<cmd>split new<cr>", desc = "Other Window", nowait = true, remap = false },
-		{
-			"<leader>/",
-			"<Plug>(comment_toggle_linewise_visual)",
-			desc = "Comment toggle linewise (visual)",
-			mode = { "v" },
-			nowait = true,
-			remap = false,
-		},
-		{ "<leader>g", mode = { "v" }, group = "Git", nowait = true, remap = false },
-		{
-			"<leader>gr",
-			"<cmd>Gitsigns reset_hunk<cr>",
-			mode = { "v" },
-			desc = "Reset Hunk",
-			nowait = true,
-			remap = false,
-		},
-		{
-			"<leader>gs",
-			"<cmd>Gitsigns stage_hunk<cr>",
-			mode = { "v" },
-			desc = "Stage Hunk",
-			nowait = true,
-			remap = false,
-		},
-	})
+	local opts = ftvim.builtin.which_key.opts
+	local vopts = ftvim.builtin.which_key.vopts
+
+	local mappings = ftvim.builtin.which_key.mappings
+	local vmappings = ftvim.builtin.which_key.vmappings
+
+	which_key.register(mappings, opts)
+	which_key.register(vmappings, vopts)
 
 	if ftvim.builtin.which_key.on_config_done then
 		ftvim.builtin.which_key.on_config_done(which_key)
