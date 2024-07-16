ecada3cf4b5504f6455f293e0856cb2d271e2c81
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-16 19:15:45 +0200

    which-key

--- lua/ftvim/core/which-key.lua
+++ lua/ftvim/core/which-key.lua
@@ -27,29 +27,11 @@ M.config = function()
 			},
 			-- add operators that will trigger motion and text object completion
 			-- to enable all native operators, set the preset / operators plugin above
-			-- operators = { gc = "Comments" },
-			key_labels = {
-				-- override the label used to display some keys. It doesn't effect WK in any other way.
-				-- For example:
-				-- ["<space>"] = "SPC",
-				-- ["<cr>"] = "RET",
-				-- ["<tab>"] = "TAB",
-			},
+			operators = { gc = "Comments" },
 			icons = {
 				breadcrumb = ftvim.icons.ui.DoubleChevronRight, -- symbol used in the command line area that shows your active key combo
 				separator = ftvim.icons.ui.BoldArrowRight, -- symbol used between a key and it's label
 				group = ftvim.icons.ui.Plus, -- symbol prepended to a group
-			},
-			popup_mappings = {
-				scroll_down = "<c-d>", -- binding to scroll down inside the popup
-				scroll_up = "<c-u>", -- binding to scroll up inside the popup
-			},
-			window = {
-				border = "single", -- none, single, double, shadow
-				position = "bottom", -- bottom, top
-				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-				winblend = 0,
 			},
 			layout = {
 				height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -57,20 +39,10 @@ M.config = function()
 				spacing = 3, -- spacing between columns
 				align = "left", -- align columns left, center or right
 			},
-			ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-			hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 			show_help = true, -- show help message on the command line when the popup is visible
 			show_keys = true, -- show the currently pressed key and its label as a message in the command line
 			triggers = "auto", -- automatically setup triggers
 			-- triggers = {"<leader>"} -- or specify a list manually
-			triggers_blacklist = {
-				-- list of mode / prefixes that should never be hooked by WhichKey
-				-- this is mostly relevant for key maps that start with a native binding
-				-- most people should not need to change this
-				i = { "j", "k" },
-				v = { "j", "k" },
-			},
-			-- disable the WhichKey popup for certain buf types and file types.
 			-- Disabled by default for Telescope
 			disable = {
 				buftypes = {},
