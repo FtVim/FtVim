0f0a7fe0747812c3a8e9be7fa09d963eafcb2ba1
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-16 19:19:48 +0200

    which-key

--- lua/ftvim/core/which-key.lua
+++ lua/ftvim/core/which-key.lua
@@ -6,6 +6,7 @@ M.config = function()
 		active = true,
 		on_config_done = nil,
 		setup = {
+			preset = "modern",
 			plugins = {
 				marks = false, -- shows a list of your marks on ' and `
 				registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
