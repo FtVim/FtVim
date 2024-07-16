3c84bf3d4af23b64e2fb51e6c1b11cd5da1189a2
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-16 21:04:07 +0200

    added copilot

--- lua/ftvim/core/copilot_cmp.lua
+++ lua/ftvim/core/copilot_cmp.lua
@@ -1,21 +1,28 @@
 local M = {}
 
-M.setup = function()
-	local ok, copilot = pcall(require, "copilot")
-	if not ok then
-		return
-	end
-
-	copilot.setup {
-		suggestion = {
-			keymap = {
-				accept = "",
-				next = "",
-				prev = "",
-				dismiss = "",
+M.config = function()
+	ftvim.builtin.copilot_cmp = {
+		setup = {
+			suggestion = {
+				keymap = {
+					accept = "",
+					next = "",
+					prev = "",
+					dismiss = "",
+				},
 			},
 		},
 	}
+end
+
+M.setup = function()
+	local ok, copilot = pcall(require, "copilot")
+	if not ok then
+		print("copilot not loaded")
+		return
+	end
+
+	copilot.config(ftvim.builtin.copilot_cmp.setup)
 
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_set_keymap("n", "", "lua require('copilot.suggestion').toggle_auto_trigger()", opts)
