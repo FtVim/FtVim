cb46d945c71cfb3ba55fd5bdad36ed5d5a6bfe41
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-19 13:25:30 +0200

    which-key please work

--- lua/ftvim/core/which-key.lua
+++ lua/ftvim/core/which-key.lua
@@ -36,11 +36,11 @@ M.config = function()
 				height = { min = 4, max = 25 }, -- min and max height of the columns
 				width = { min = 20, max = 50 }, -- min and max width of the columns
 				spacing = 3, -- spacing between columns
-				align = "left", -- align columns left, center or right
+				align = "center", -- align columns left, center or right
 			},
 			show_help = true, -- show help message on the command line when the popup is visible
 			show_keys = true, -- show the currently pressed key and its label as a message in the command line
-			triggers = "auto", -- automatically setup triggers
+			triggers = { "<leader>" }, -- automatically setup triggers
 			-- triggers = {"<leader>"} -- or specify a list manually
 			-- Disabled by default for Telescope
 			disable = {
