b5dd436583a2d47e02371e25dbdd39872c62c261
Bautista Rodriguez <bautirodri@gmail.com>
2024-07-19 13:21:42 +0200

    which-key please work

--- lua/ftvim/plugins/init.lua
+++ lua/ftvim/plugins/init.lua
@@ -6,6 +6,9 @@ local plugins = require("ftvim.plugins.core-plugins")
 require("ftvim.core.theme").setup()
 require("ftvim.plugin-loader").load({ plugins, ftvim.plugins })
 
+vim.g.mapleader = (ftvim.leader == "space" and " ") or ftvim.leader
+vim.g.maplocalleader = " "
+
 return {
 	{ "folke/lazy.nvim", version = "*" },
 	{ "FtVim/FtVim", priority = 10000, lazy = false, version = "*" },
