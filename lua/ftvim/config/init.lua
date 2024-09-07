9baafd682d90325b2086d1cf0f9a06f719abdcff
San-tito <68120901+San-tito@users.noreply.github.com>
2024-09-07 14:07:06 +0200

    okokok is cooking

--- lua/ftvim/config/init.lua
+++ lua/ftvim/config/init.lua
@@ -1,1 +1,314 @@
 _G.FtVim = require("ftvim.util")
+
+---@class FtVimConfig: FtVimOptions
+local M = {}
+
+M.version = "0.0.0" 
+FtVim.config = M
+
+---@class FtVimOptions
+local defaults = {
+  -- colorscheme can be a string like `catppuccin` or a function that will load the colorscheme
+  ---@type string|fun()
+  colorscheme = function()
+    require("tokyonight").load()
+  end,
+  -- load the default settings
+  defaults = {
+    autocmds = true, -- ftvim.config.autocmds
+    keymaps = true, -- ftvim.config.keymaps
+    -- ftvim.config.options can't be configured here since that's loaded before ftvim setup
+    -- if you want to disable loading options, add `package.loaded["ftvim.config.options"] = true` to the top of your init.lua
+  },
+  news = {
+    -- When enabled, NEWS.md will be shown when changed.
+    -- This only contains big new features and breaking changes.
+    ftvim = true,
+    -- Same but for Neovim's news.txt
+    neovim = false,
+  },
+  -- icons used by other plugins
+  -- stylua: ignore
+  icons = {
+    misc = {
+      dots = "󰇘",
+    },
+    ft = {
+      octo = "",
+    },
+    dap = {
+      Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+      Breakpoint          = " ",
+      BreakpointCondition = " ",
+      BreakpointRejected  = { " ", "DiagnosticError" },
+      LogPoint            = ".>",
+    },
+    diagnostics = {
+      Error = " ",
+      Warn  = " ",
+      Hint  = " ",
+      Info  = " ",
+    },
+    git = {
+      added    = " ",
+      modified = " ",
+      removed  = " ",
+    },
+    kinds = {
+      Array         = " ",
+      Boolean       = "󰨙 ",
+      Class         = " ",
+      Codeium       = "󰘦 ",
+      Color         = " ",
+      Control       = " ",
+      Collapsed     = " ",
+      Constant      = "󰏿 ",
+      Constructor   = " ",
+      Copilot       = " ",
+      Enum          = " ",
+      EnumMember    = " ",
+      Event         = " ",
+      Field         = " ",
+      File          = " ",
+      Folder        = " ",
+      Function      = "󰊕 ",
+      Interface     = " ",
+      Key           = " ",
+      Keyword       = " ",
+      Method        = "󰊕 ",
+      Module        = " ",
+      Namespace     = "󰦮 ",
+      Null          = " ",
+      Number        = "󰎠 ",
+      Object        = " ",
+      Operator      = " ",
+      Package       = " ",
+      Property      = " ",
+      Reference     = " ",
+      Snippet       = " ",
+      String        = " ",
+      Struct        = "󰆼 ",
+      TabNine       = "󰏚 ",
+      Text          = " ",
+      TypeParameter = " ",
+      Unit          = " ",
+      Value         = " ",
+      Variable      = "󰀫 ",
+    },
+  },
+  ---@type table<string, string[]|boolean>?
+  kind_filter = {
+    default = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      "Package",
+      "Property",
+      "Struct",
+      "Trait",
+    },
+    markdown = false,
+    help = false,
+    -- you can specify a different filter for each filetype
+    lua = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      -- "Package", -- remove package since luals uses it for control flow structures
+      "Property",
+      "Struct",
+      "Trait",
+    },
+  },
+}
+
+M.json = {
+  version = 6,
+  path = vim.g.ftvim_json or vim.fn.stdpath("config") .. "/ftvim.json",
+  data = {
+    version = nil, ---@type string?
+    news = {}, ---@type table<string, string>
+    extras = {}, ---@type string[]
+  },
+}
+
+function M.json.load()
+  local f = io.open(M.json.path, "r")
+  if f then
+    local data = f:read("*a")
+    f:close()
+    local ok, json = pcall(vim.json.decode, data, { luanil = { object = true, array = true } })
+    if ok then
+      M.json.data = vim.tbl_deep_extend("force", M.json.data, json or {})
+      if M.json.data.version ~= M.json.version then
+        FtVim.json.migrate()
+      end
+    end
+  end
+end
+
+---@type FtVimOptions
+local options
+local clipboard
+
+---@param opts? FtVimOptions
+function M.setup(opts)
+  options = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
+
+  -- autocmds can be loaded lazily when not opening a file
+  local autocmds = vim.fn.argc(-1) == 0
+  if not autocmds then
+    M.load("autocmds")
+  end
+
+  local group = vim.api.nvim_create_augroup("FtVim", { clear = true })
+  vim.api.nvim_create_autocmd("User", {
+    group = group,
+    pattern = "VeryLazy",
+    callback = function()
+      if lazy_autocmds then
+        M.load("autocmds")
+      end
+      M.load("keymaps")
+      if clipboard ~= nil then
+        vim.opt.clipboard = clipboard
+      end
+
+      FtVim.format.setup()
+      FtVim.news.setup()
+      FtVim.root.setup()
+
+      vim.api.nvim_create_user_command("FtExtras", function()
+        FtVim.extras.show()
+      end, { desc = "Manage FtVim extras" })
+
+      vim.api.nvim_create_user_command("FtHealth", function()
+        vim.cmd([[Lazy! load all]])
+        vim.cmd([[checkhealth]])
+      end, { desc = "Load all plugins and run :checkhealth" })
+
+      local health = require("lazy.health")
+      vim.list_extend(health.valid, {
+        "recommended",
+        "desc",
+        "vscode",
+      })
+    end,
+  })
+
+  FtVim.track("colorscheme")
+  FtVim.try(function()
+    if type(M.colorscheme) == "function" then
+      M.colorscheme()
+    else
+      vim.cmd.colorscheme(M.colorscheme)
+    end
+  end, {
+    msg = "Could not load your colorscheme",
+    on_error = function(msg)
+      LazyVim.error(msg)
+      vim.cmd.colorscheme("habamax")
+    end,
+  })
+  FtVim.track()
+end
+
+---@param buf? number
+---@return string[]?
+function M.get_kind_filter(buf)
+  buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
+  local ft = vim.bo[buf].filetype
+  if M.kind_filter == false then
+    return
+  end
+  if M.kind_filter[ft] == false then
+    return
+  end
+  if type(M.kind_filter[ft]) == "table" then
+    return M.kind_filter[ft]
+  end
+  ---@diagnostic disable-next-line: return-type-mismatch
+  return type(M.kind_filter) == "table" and type(M.kind_filter.default) == "table" and M.kind_filter.default or nil
+end
+
+---@param name "autocmds" | "options" | "keymaps"
+function M.load(name)
+  local function _load(mod)
+    if require("lazy.core.cache").find(mod)[1] then
+      FtVim.try(function()
+        require(mod)
+      end, { msg = "Failed loading " .. mod })
+    end
+  end
+  local pattern = "FtVim" .. name:sub(1, 1):upper() .. name:sub(2)
+  -- always load ftvim, then user file
+  if M.defaults[name] or name == "options" then
+    _load("ftvim.config." .. name)
+    vim.api.nvim_exec_autocmds("User", { pattern = pattern .. "Defaults", modeline = false })
+  end
+  _load("config." .. name)
+  if vim.bo.filetype == "lazy" then
+    -- HACK: FtVim may have overwritten options of the Lazy ui, so reset this here
+    vim.cmd([[do VimResized]])
+  end
+  vim.api.nvim_exec_autocmds("User", { pattern = pattern, modeline = false })
+end
+
+M.did_init = false
+function M.init()
+  if M.did_init then
+    return
+  end
+  M.did_init = true
+  local plugin = require("lazy.core.config").spec.plugins.FtVim
+  if plugin then
+    vim.opt.rtp:append(plugin.dir)
+  end
+
+  package.preload["ftvim.plugins.lsp.format"] = function()
+    FtVim.deprecate([[require("ftvim.plugins.lsp.format")]], [[FtVim.format]])
+    return FtVim.format
+  end
+
+  -- delay notifications till vim.notify was replaced or after 500ms
+  FtVim.lazy_notify()
+
+  -- load options here, before lazy init while sourcing plugin modules
+  -- this is needed to make sure options will be correctly applied
+  -- after installing missing plugins
+  M.load("options")
+  -- defer built-in clipboard handling: "xsel" and "pbcopy" can be slow
+  clipboard = vim.opt.clipboard
+  vim.opt.clipboard = ""
+
+  if vim.g.deprecation_warnings == false then
+    vim.deprecate = function() end
+  end
+
+  FtVim.plugin.setup()
+  M.json.load()
+end
+
+setmetatable(M, {
+  __index = function(_, key)
+    if options == nil then
+      return vim.deepcopy(defaults)[key]
+    end
+    ---@cast options LazyVimConfig
+    return options[key]
+  end,
+})
+
+return M
