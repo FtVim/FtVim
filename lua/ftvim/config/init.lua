53eb0542984f577edae07acd7733a190187214c6
San-tito <68120901+San-tito@users.noreply.github.com>
2024-10-12 13:40:57 +0200

    maybe

--- lua/ftvim/config/init.lua
+++ lua/ftvim/config/init.lua
@@ -1,46 +1,19 @@
-_G.Ftvim = require("ftvim.util")
-
----@class FtvimConfig: FtvimOptions
 local M = {}
 
-M.version = "0.0.1" -- x-release-please-version
-Ftvim.config = M
-
----@class FtvimOptions
 local defaults = {
-  -- colorscheme can be a string like `catppuccin` or a function that will load the colorscheme
-  ---@type string|fun()
   colorscheme = function()
     require("tokyonight").load()
   end,
-  -- load the default settings
   defaults = {
-    autocmds = true, -- ftvim.config.autocmds
-    keymaps = true, -- ftvim.config.keymaps
-    -- ftvim.config.options can't be configured here since that's loaded before ftvim setup
-    -- if you want to disable loading options, add `package.loaded["ftvim.config.options"] = true` to the top of your init.lua
+    autocmds = true,
+    keymaps = true,
   },
-  -- icons used by other plugins
-  -- stylua: ignore
   icons = {
     misc = {
       dots = "󰇘",
     },
     ft = {
       octo = "",
-    },
-    dap = {
-      Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
-      Breakpoint          = " ",
-      BreakpointCondition = " ",
-      BreakpointRejected  = { " ", "DiagnosticError" },
-      LogPoint            = ".>",
-    },
-    diagnostics = {
-      Error = " ",
-      Warn  = " ",
-      Hint  = " ",
-      Info  = " ",
     },
     git = {
       added    = " ",
@@ -86,115 +59,11 @@ local defaults = {
       TypeParameter = " ",
       Unit          = " ",
       Value         = " ",
-      Variable      = "󰀫 ",
-    },
-  },
-  ---@type table<string, string[]|boolean>?
-  kind_filter = {
-    default = {
-      "Class",
-      "Constructor",
-      "Enum",
-      "Field",
-      "Function",
-      "Interface",
-      "Method",
-      "Module",
-      "Namespace",
-      "Package",
-      "Property",
-      "Struct",
-      "Trait",
-    },
-    markdown = false,
-    help = false,
-    -- you can specify a different filter for each filetype
-    lua = {
-      "Class",
-      "Constructor",
-      "Enum",
-      "Field",
-      "Function",
-      "Interface",
-      "Method",
-      "Module",
-      "Namespace",
-      "Property",
-      "Struct",
-      "Trait",
+      Variable      = " ",
     },
   },
 }
 
----@type FtvimOptions
-local options
-local clipboard
-
----@param opts? FtvimOptions
-function M.setup(opts)
-  options = vim.tbl_deep_extend("force", defaults, opts or {}) or {}
-
-  -- autocmds can be loaded lazily when not opening a file
-  local autocmds = vim.fn.argc(-1) == 0
-  if not autocmds then
-    M.load("autocmds")
-  end
-
-  local group = vim.api.nvim_create_augroup("Ftvim", { clear = true })
-  vim.api.nvim_create_autocmd("User", {
-    group = group,
-    pattern = "VeryLazy",
-    callback = function()
-      if autocmds then
-        M.load("autocmds")
-      end
-      M.load("keymaps")
-      if clipboard ~= nil then
-        vim.opt.clipboard = clipboard
-      end
-
-      Ftvim.format.setup()
-      Ftvim.news.setup()
-      Ftvim.root.setup()
-    end,
-  })
-
-  Ftvim.track("colorscheme")
-  Ftvim.try(function()
-    if type(M.colorscheme) == "function" then
-      M.colorscheme()
-    else
-      vim.cmd.colorscheme(M.colorscheme)
-    end
-  end, {
-    msg = "Could not load your colorscheme",
-    on_error = function(msg)
-      Ftvim.error(msg)
-      vim.cmd.colorscheme("habamax")
-    end,
-  })
-  Ftvim.track()
-end
-
----@param buf? number
----@return string[]?
-function M.get_kind_filter(buf)
-  buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
-  local ft = vim.bo[buf].filetype
-  if M.kind_filter == false then
-    return
-  end
-  if M.kind_filter[ft] == false then
-    return
-  end
-  if type(M.kind_filter[ft]) == "table" then
-    return M.kind_filter[ft]
-  end
-  ---@diagnostic disable-next-line: return-type-mismatch
-  return type(M.kind_filter) == "table" and type(M.kind_filter.default) == "table" and M.kind_filter.default or nil
-end
-
----@param name "autocmds" | "options" | "keymaps"
 function M.load(name)
   local function _load(mod)
     if require("lazy.core.cache").find(mod)[1] then
@@ -203,7 +72,6 @@ function M.load(name)
       end, { msg = "Failed loading " .. mod })
     end
   end
-  -- always load ftvim, then user file
   if M.defaults[name] or name == "options" then
     _load("ftvim.config." .. name)
   end
@@ -216,24 +84,7 @@ function M.init()
     return
   end
   M.did_init = true
-
-  -- load options here
   M.load("options")
-  -- defer built-in clipboard handling: "xsel" and "pbcopy" can be slow
-  clipboard = vim.opt.clipboard
-  vim.opt.clipboard = ""
-
-  --Ftvim.plugin.setup()
 end
-
-setmetatable(M, {
-  __index = function(_, key)
-    if options == nil then
-      return vim.deepcopy(defaults)[key]
-    end
-    ---@cast options FtvimConfig
-    return options[key]
-  end,
-})
 
 return M
