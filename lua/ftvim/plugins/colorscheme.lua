78c76d1660ee240acb3b82fed9608ddd35e5b129
Bautista Rodriguez <bautirodri@gmail.com>
2024-10-18 15:49:26 +0200

    added telescope-colorscheme-persist plugin

--- lua/ftvim/plugins/colorscheme.lua
+++ lua/ftvim/plugins/colorscheme.lua
@@ -1,4 +1,14 @@
 return {
+  {
+	"tingey21/telescope-colorscheme-persist.nvim",
+	dependencies = { "nvim-telescope/telescope.nvim" },
+	lazy = false,
+	config = function()
+		require("telescope-colorscheme-persist").setup({
+			keybind = "<leader>ft"
+		})
+	end,
+  },
 
   -- vogsphere
   {
